6dcd3c506d428e2b51f39942566ad8249f2503d3
Marco <39marcom@gmail.com>
2025-07-02 14:44:48 -0700

    create navbar

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+export default function Navbar() {
+  return (
+    <div className="flex fixed top-0 left-0 w-full z-50  justify-between px-8 py-4">
+      <div className="flex gap-6 text-[1.5rem] font-medium ">
+        <Link
+          href="/"
+          className="hover:text-[var(--secondary)] transition-colors duration-500"
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="hover:text-[var(--secondary)] transition-colors duration-500"
+        >
+          About Me
+        </Link>
+        <Link
+          href="/projects"
+          className="hover:text-[var(--secondary)] transition-colors duration-500"
+        >
+          Personal Projects
+        </Link>
+        <Link
+          href="/contact"
+          className="hover:text-[var(--secondary)] transition-colors duration-500"
+        >
+          Contact Me
+        </Link>
+      </div>
+      <div className="flex gap-2 ">
+        <a
+          href="https://www.linkedin.com/in/mmaldonado2/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInLogoIcon className="w-[2rem] h-[2rem]" />
+        </a>
+        <a
+          href="https://github.com/marcopolo39"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubLogoIcon className="w-[2rem] h-[2rem]" />
+        </a>
+      </div>
+    </div>
+  );
+}
