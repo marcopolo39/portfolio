99900a21e9400c161247d782db5f9ccc2f0e032e
Marco <39marcom@gmail.com>
2025-07-10 11:13:48 -0700

    created sanity schema and routing for queries

--- portfolio/src/app/projects/[slug]/page.js
+++ portfolio/src/app/projects/[slug]/page.js
@@ -2,20 +2,16 @@ import Navbar from "@/components/Navbar";
 import { client } from "@/sanity/lib/client";
 import { defineQuery } from "next-sanity";
 
-const PROJECTS_QUERY = defineQuery(
-  `*[_type == 'projects'  && defined(slug.current)] |{title, description}`
-);
+const PROJECT_DATA_QUERY = `*[_type == "projects" && slug.current == $slug][0]`;
 
-export default async function Projects() {
-  const data = await client.fetch(PROJECTS_QUERY);
-
+export default async function ProjectView({ params }) {
+  const data = await client.fetch(PROJECT_DATA_QUERY, await params);
   console.log(data);
-
   return (
     <div className="flex justify-center w-full">
       <Navbar />
       <div className="flex flex-col items-center min-h-screen pt-25 px-15 max-w-4xl gap-6 w-full">
-        Projects
+        Detailed Project View
       </div>
     </div>
   );
