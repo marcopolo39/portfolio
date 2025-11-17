e9eb384a21c884f2fc6e48f33f4b7e4468586b47
Marco <39marcom@gmail.com>
2025-11-16 21:49:34 -0800

    updated coming soon placeholder

--- portfolio/src/app/contact/page.js
+++ portfolio/src/app/contact/page.js
@@ -1,3 +1,14 @@
+import Navbar from "@/components/Navbar";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="flex justify-center w-full">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen pt-25 px-4">
+        <div className="bg-[var(--secondary)]/30 backdrop-blur-md rounded-[25px] px-8 py-6 border border-white/20 shadow-lg">
+          <div className="text-[3rem] text-center mb-2">Coming Soon!</div>
+        </div>
+      </div>
+    </div>
+  );
 }
